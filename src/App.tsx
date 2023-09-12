332238b8b0ec829f2f536e121887dec73984bcb2
UmedIrgashev <umedjon.irgashev@bk.ru>
2023-09-12 22:29:16 +0500

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,263 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* HEADER */}
+      <header>
+        <div className="block ">
+          <img className="img-1" src="/src/img/logo-2.png" alt="" />
+
+          <div>
+            <h1>
+              Why stay hungry when <br />
+              you can order from DialEats
+            </h1>
+            <p>Dowload the bella onoje's food app now on</p>
+            <div className="div-btn">
+              <button className="btn1">
+                <img src="/src/img/Group.png" alt="" />
+                <div>
+                  <span className="spn-2">Download on the</span>
+                  <br />
+                  <span className="spn-1">App Store</span>
+                </div>
+              </button>
+              <button className="btn2">
+                <img src="/src/img/google-play 1.png" alt="" />
+                <div>
+                  <span className="spn-2">Andriod App On</span>
+                  <br />
+                  <span className="spn-1">Google Play</span>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="block block-2">
+          <p>Privacy Policy</p>
+
+          <div className="phone-img">
+            <img
+              // data-aos="fade-down"
+              // data-aos-duration="90000"
+              className="phone-img-1"
+              src="/src/img/Rectangle4 1.png"
+              alt=""
+            />
+            <img className="phone-img-2" src="/src/img/Group 53.png" alt="" />
+          </div>
+        </div>
+      </header>
+      {/* MAIN */}
+      <main>
+        <div className="block">
+          <img src="/src/img/Group 61.png" alt="" />
+        </div>
+        <div className="block">
+          <img src="/src/img/Group 64.png" alt="" />
+        </div>
+        <div className="block">
+          <img src="/src/img/Group 66.png" alt="" />
+        </div>
+        <div className="block">
+          <img src="/src/img/Group 65.png" alt="" />
+        </div>
+      </main>
+      <hr />
+      {/* CONTAINER */}
+      <div className="container">
+        <h1 className="main-h-1">How the app works</h1>
+        <div className="container-1">
+          <div className="block">
+            <img src="/src/img/LOGIN 2.png" alt="" />
+          </div>
+          <div className="block2">
+            <h3>Create an account</h3>
+            <h2>Create/login to an existing account to get started</h2>
+            <h4>
+              An account is created with your email and a desired password
+            </h4>
+          </div>
+        </div>
+        <div className="container-1">
+          <div className="block2">
+            <h3>Explore varieties</h3>
+            <h2>Shop for your favorites meal as e dey hot.</h2>
+            <h4>
+              Shop for your favorite meals or drinks and enjoy while doing it.
+            </h4>
+          </div>
+          <div className="block">
+            <img src="/src/img/Rectangle1 2.png" alt="" />
+          </div>
+        </div>
+        <div className="container-1">
+          <div className="block">
+            <img src="/src/img/CHECKOUT 2.png" alt="" />
+          </div>
+          <div className="block2">
+            <h3>Checkout</h3>
+            <h2>When you done check out and get it delivered.</h2>
+            <h4>When you done check out and get it delivered with ease.</h4>
+          </div>
+        </div>
+        <img className="elipse-1" src="/src/img/Ellipse 10.png" alt="" />
+        <img className="elipse-2" src="/src/img/Ellipse 9.png" alt="" />
+      </div>
+      <hr />
+      {/* MAiN */}
+      <h1 className="main-h-1">What others are saying</h1>
+      <div className="container-2">
+        <div className="block">
+          <p>
+            Completely beautiful website and amazing support! This is my second
+            website from this author and I love both of the sites so much and
+            she has helped me so well when I needed it!
+          </p>
+          <img src="/src/img/image 5.png" alt="" />
+          <h3>Happy User</h3>
+          <p>tempy.club</p>
+        </div>
+        <div className="block">
+          <p>
+            Really easy to use and customize. easy to understand. Darinka helped
+            me with my site! She answers very quickly, a good service! Thank you
+            very much!
+          </p>
+          <img src="/src/img/image 6.png" alt="" />
+          <h3>Happy User</h3>
+          <p>tempy.club</p>
+        </div>
+        <div className="block">
+          <p>
+            Really pleasing to look at! Documentation (including installation
+            instructions) are clear and that is coming from a relatively new
+            WordPress user.
+          </p>
+          <img src="/src/img/image 7.png" alt="" />
+          <h3>Happy User</h3>
+          <p>tempy.club</p>
+        </div>
+      </div>
+      {/* CONTAINER-3 */}
+      <div className="container-3">
+        <div className="block">
+          <h2>Contact Us</h2>
+          <p>
+            Availble in your favorite store. Start your premium experince now.
+          </p>
+          <span>
+            <input type="Email" placeholder="Email" />
+            <button>Contact me</button>
+          </span>
+          <div className="div-btn">
+            <button className="btn1">
+              <img src="/src/img/Group.png" alt="" />
+              <div>
+                <span className="spn-2">Download on the</span>
+                <br />
+                <span className="spn-1">App Store</span>
+              </div>
+            </button>
+            <button className="btn2">
+              <img src="/src/img/google-play 1.png" alt="" />
+              <div>
+                <span className="spn-2">Andriod App On</span>
+                <br />
+                <span className="spn-1">Google Play</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* FOOTER */}
+      <footer>
+        <h1>FAQ</h1>
+        <div className="container-5">
+          <div className="block">
+            <img
+              src="/src/img/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle 1.png"
+              alt=""
+            />
+            <div>
+              <p>
+                <span className="footer-span-1">
+                  How to add a booking Source
+                </span>
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+              <br />
+              <span className="footer-span-2">READ ME</span>
+            </div>
+          </div>
+          <div className="block">
+            <img
+              src="/src/img/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle 1.png"
+              alt=""
+            />
+            <div>
+              <p>
+                <span className="footer-span-1">Rate Plan Settings</span>
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+              <br />
+              <span className="footer-span-2">READ ME</span>
+            </div>
+          </div>
+          <div className="block">
+            <img
+              src="/src/img/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle 1.png"
+              alt=""
+            />
+            <div>
+              <p>
+                <span className="footer-span-1">Ordering Food</span>
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+              <br />
+              <span className="footer-span-2">READ ME</span>
+            </div>
+          </div>
+          <div className="block">
+            <img
+              src="/src/img/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle 1.png"
+              alt=""
+            />
+            <div>
+              <p>
+                <span className="footer-span-1">Rate Plan Settings</span>
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
+              <br />
+              <span className="footer-span-2">READ ME</span>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="footer-end">
+        <img src="/src/img/app-icon9 1.png" alt="" />
+        <span>Copywright 2021 Dialeats.com </span>
+        <div className="icon">
+          <img src="/src/img/facebook-logo.png" alt="" />
+          <img src="/src/img/Vector.png" alt="" />
+          <img src="/src/img/instagram (1) 1.png" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
